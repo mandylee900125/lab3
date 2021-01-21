e1766207c6b9425a3106c93f66e504aba680130d
leem3209@gmail.com <leem3209@gmail.com>
2021-01-22 02:37:14 +0800

    lab3

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,42 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+	$('.jumbotron h1').text("Javascript has taken control");
+	$(".jumbotron p").toggleClass("active");
+	$(".jumbotron p").addClass("active");
+	$("#testjs").text("Please wait...");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+
+	$("a.thumbnail").click(projectClick);
 }
+
+function projectClick(e){
+	e.preventDefault();
+	// $(this).css("background-color", "#7fff00");
+	var containingProject = $(this).closest(".project");
+ //    containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+ 	var description = $(containingProject).find(".project-description");
+ 	
+ 	if(description.length == 0){
+ 		$(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+ 	}
+ 	else {
+    	$(description).fadeOut()
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
